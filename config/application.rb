00ced21311f13318fa01f71228e3436834e6ecdb
Pato <pevargas@valdosta.edu>
2016-10-27 01:31:45 -0400

    thingy added to application

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module LogIn
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_dispatch.cookies_serializer = :hybrid
   end
 end
